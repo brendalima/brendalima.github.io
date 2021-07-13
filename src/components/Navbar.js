c6935beb55485417b738ce9690c7898a27d61f42
brendalima <brendalima@>
2021-07-12 23:03:01 -0300

    Adjusting classes in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <div className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Brenda Lima</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse ml-5" id="navbarSupportedContent">
+        <ul className="navbar-nav">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
           </li>
@@ -22,7 +26,7 @@ function Navbar() {
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   )
 }
 
